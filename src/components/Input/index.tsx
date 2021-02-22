42d8b4f3fd74a5488f0e85b16c56da57698e1225
Vinicius Colares <dev@viniciuscolares.com>
2021-02-22 19:53:20 -0300

    feat(contact): newsletter integration

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,40 @@
+import { colors } from '@styles/theme'
 import { MotionProps } from 'framer-motion'
+import { BiErrorAlt } from 'react-icons/bi'
+
 import * as $ from './styles'
 
 const Input = ({
-  placeholder,
+  value,
   name,
+  state,
+  error,
+  placeholder,
   type = 'text',
   ...restProps
 }: InputProps & React.HTMLAttributes<HTMLInputElement> & MotionProps) => {
   return (
-    <$.Input name={name} type={type} placeholder={placeholder} {...restProps} />
+    <$.Container>
+      <$.Input
+        type={type}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        state={state}
+        {...restProps}
+      />
+      {error && <BiErrorAlt size={26} color={colors.error} />}
+    </$.Container>
   )
 }
 
 export interface InputProps {
+  value: string
   name: string
+  error?: string
   type?: string
   placeholder: string
+  state?: string
 }
 
 export default Input
