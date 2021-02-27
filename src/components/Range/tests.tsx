93571244947aff8e3e8c5c5bf6286ad16928c53f
Vinicius Colares <dev@viniciuscolares.com>
2021-02-27 00:01:00 -0300

    fix(range): fixed test

--- src/components/Range/tests.tsx
+++ src/components/Range/tests.tsx
@@ -1,12 +1,12 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import Range from '.'
 
 describe('<Range />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Range />)
-
-    expect(screen.getByRole('heading', { name: /Range/i })).toBeInTheDocument()
+    const { container } = render(
+      <Range from="0" to="10" value="5" onSlide={() => console.log()} />
+    )
 
     expect(container.firstChild).toMatchSnapshot()
   })
